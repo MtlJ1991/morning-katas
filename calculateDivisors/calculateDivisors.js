566d7707cc2931a2e0c1ee182bfdc8950711e8a7
MtlJ1991 <MtlJ1991@gmail.com>
2018-01-31 13:55:29 +0000

    pigLatin and calculateDivisors passing

--- calculateDivisors/calculateDivisors.js
+++ calculateDivisors/calculateDivisors.js
@@ -1,6 +1,14 @@
 var calculateDivisors = function (number) {
-    // Add some new comments to
-    // check the commit history
+  if (number < 3) return 0;
+  
+  let divisorsOfNumber = 0;
+  
+  for (let i = 0; i < number; i ++) {
+    if (i % 3 === 0 || i % 5 === 0) divisorsOfNumber += i;
+  }
+  return divisorsOfNumber;
+
+  
   };
   
   if (typeof module !== 'undefined') {
--- pigLatin/pigLatin.js
+++ pigLatin/pigLatin.js
@@ -1,9 +1,17 @@
 // Please do not change the name of this function
 var pigLatin = function (str) {
-  str = str.toLowerCase()
-
-  return str;
-};
+  let regex = /[aeiou]/gi;
+  
+  let result = str.split(' ').map((element) => {
+    if (element[0].match(regex)) return element + 'way';
+  
+    else {let vowelIndice = element.indexOf(element.match(regex)[0]);
+     return element.substr(vowelIndice) + element.substr(0, vowelIndice) + 'ay';
+    } 
+  }).join(' ');
+  return result;
+  };
+  
   
   if (typeof module !== 'undefined') {
     module.exports = {pigLatin};
